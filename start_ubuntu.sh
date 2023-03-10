3141728ab2eef04d914e4db7927669523f0632b8
Dmitrii_Antonov <antonov.buscorr@gmail.com>
2023-03-10 16:43:17 +0100

    Auto commit from script

--- start_ubuntu.sh
+++ start_ubuntu.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-## create ansible user
-
+## create technical ansible user
 useradd  -m ansible -s /bin/bash
+## grant ansible user get sudo without password
 echo "ansible ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
-su ansible
-echo $SUER
+## update repositories and system packages
+apt update
+apt -y upgrade
+## install ansible
+apt -y install ansible
+## put default ansible config to ansible home directory
+tee -a >> /home/ansible/ansible.cfg << EOF
+[privilege_escalation]
+become = True
+become_method = sudo
+become_user = root
+become_ask_pass = False
+EOF
+## copy start playbook from current directory to ansible home directory
+cp ./start_playbook.yml /home/ansible/start_playbook.yml
+## change owner to ansible
+chown ansible:ansible /home/ansible/start_playbook.yml
+## run start playbok as ansible user
+sudo -u ansible -H sh -c "cd ~; ansible-playbook start_playbook.yml"
