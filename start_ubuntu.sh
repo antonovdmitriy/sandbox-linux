1922adc2c6f15f3068954ad1bf66585acc604560
Dmitriy Antonov <antonov.buscorr@gmail.com>
2023-05-11 19:19:34 +0200

    #5 #30 Secrets path to arguments and add logging (#31)

--- start_ubuntu.sh
+++ start_ubuntu.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Start timer
+start=$(date +%s)
+
+# Define log levels
+INFO="\033[1;34mINFO:\033[0m"
+ERROR="\033[1;31mERROR:\033[0m"
+
 # File name
 PROGNAME=$(basename "$0")
+# Default values
 environment=java
 java_playbook=./start_playbook_java.yml
 scala_playbook=./start_playbook_scala.yml
+secrets_file=/mnt/hgfs/secrets/secret.yml
+verbose=""
 
 usage() {
 	echo "Script for prepare linux enviromment for development"
@@ -19,11 +29,16 @@ usage() {
 	echo "  -e <environemt>, --environment <environment>"
 	echo "      Possible environment: java, scala. Java is default"
 	echo
+	echo "  -s <secrets>, --secrets <secrets>"
+	echo "      Path to secrets file. Default is /mnt/hgfs/secrets/secret.yml"
+	echo
+	echo "  -v, --verbose"
+	echo "      Enable verbose output (-vv) in Ansible."	
+	echo
 	echo "  --"
 	echo "      Do not interpret any more arguments as options."
 	echo
 }
-
 
 while [ "$#" -gt 0 ]
 do
@@ -36,18 +51,24 @@ do
 		environment="$2"
 		shift
 		;;
+	-s|--secrets)
+		secrets_file="$2"
+		shift
+		;;
+	-v|--verbose)
+		verbose="-vv"
+		;;		
 	--)
 		break
 		;;
 	-*)
-		echo "Invalid option '$1'. Use --help to see the valid options" >&2
+		echo -e "$ERROR Invalid option '$1'. Use --help to see the valid options" >&2
 		exit 1
 		;;
 	*)	;;
 	esac
 	shift
 done
-
 
 case $environment in
 java)
@@ -57,8 +78,15 @@ scala)
   playbook_to_start=$scala_playbook
   ;;
 *)
-  echo 'wrong envinoment. see --help' >&2; exit 1
+  echo -e "$ERROR wrong environment. see --help" >&2; exit 1
   ;;
 esac
 
-sudo ./sudo-actions.sh $playbook_to_start
+echo -e "$INFO Starting sudo actions with playbook: $playbook_to_start and secrets file: $secrets_file"
+sudo ./sudo-actions.sh "$playbook_to_start" "$secrets_file" "$verbose"
+
+# End timer
+end=$(date +%s)
+runtime=$((end-start))
+
+echo -e "$INFO Done. Execution time: $runtime seconds"
--- sudo-actions.sh
+++ sudo-actions.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then echo "$1" argument for playbook is empty >&2; exit 1; fi
-if [ ! -f "$1" ]; then echo "$1" playbook does not exist >&2; exit 2; fi
+# Define log levels
+INFO="\033[1;34mINFO:\033[0m"
+ERROR="\033[1;31mERROR:\033[0m"
 
-# create technical ansible user
-useradd  -m ansible -s /bin/bash
-# grant ansible user get sudo without password
-echo "ansible ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
-# update repositories and system packages
-apt update
-apt -y upgrade
-# clean old packages and journal
-apt -y autoremove
-apt -y autoclean
-journalctl --vacuum-time=1d
-## install ansible
-apt -y install ansible
-## put default ansible config to ansible home directory
+if [ -z "$1" ]; then echo -e "$ERROR First argument (playbook path) is empty" >&2; exit 1; fi
+if [ ! -f "$1" ]; then echo -e "$ERROR Playbook $1 does not exist" >&2; exit 2; fi
+if [ -z "$2" ]; then echo -e "$ERROR Second argument (secrets path) is empty" >&2; exit 3; fi
+if [ ! -f "$2" ]; then echo -e "$ERROR Secrets file $2 does not exist" >&2; exit 4; fi
+
+echo -e "$INFO Creating technical ansible user..."
+useradd  -m ansible -s /bin/bash > /dev/null
+
+echo -e "$INFO Granting ansible user get sudo without password..."
+echo "ansible ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers > /dev/null
+
+echo -e "$INFO Updating repositories and system packages..."
+apt update > /dev/null
+apt -y upgrade > /dev/null
+
+echo -e "$INFO Cleaning old packages and journal..."
+apt -y autoremove > /dev/null
+apt -y autoclean > /dev/null
+journalctl --vacuum-time=1d > /dev/null
+
+echo -e "$INFO Installing ansible..."
+apt -y install ansible > /dev/null
+
+echo -e "$INFO Configuring ansible..."
 tee -a >> /home/ansible/ansible.cfg << EOF
 [privilege_escalation]
 become = True
@@ -24,18 +35,18 @@ become_method = sudo
 become_user = root
 become_ask_pass = False
 EOF
-change gnome terminal font size
 
-# copy start playbook from current directory to ansible home directory
+echo -e "$INFO Copying start playbook to ansible home directory..."
 cp "$1" /home/ansible/start_playbook.yml
-## change owner to ansible
 chown ansible:ansible /home/ansible/start_playbook.yml
-# copy roles directory from source directory to ansible home directory
+
+echo -e "$INFO Copying roles directory to ansible home directory..."
 cp -R ./roles/ /home/ansible/roles
-# change owner to ansible for roles directory and its contents
 chown -R ansible:ansible /home/ansible/roles
-## run start playbok as ansible user
-sudo -u ansible -H sh -c "cd ~; ansible-playbook start_playbook.yml -vv -e @/mnt/hgfs/secrets/secret.yml --ask-vault-pass"
-## delete technical ansible user
+
+echo -e "$INFO Running start playbook as ansible user..."
+sudo -u ansible -H sh -c "cd ~; ansible-playbook start_playbook.yml $3 -e @$2 --ask-vault-pass"
+
+echo -e "$INFO Deleting technical ansible user..."
 userdel -r ansible
 sed -i '/ansible/d' /etc/sudoers
